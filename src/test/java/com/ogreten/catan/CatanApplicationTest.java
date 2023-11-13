5934344e102a9195afb78f18d84c874d6d377a1b
Ahmet Can Ogreten <ahmeetcanogreten@gmail.com>
2023-11-13 05:58:43 +0300

    Test problem is fixed by providing vscode env variables for testing

---
+++ src/test/java/com/ogreten/catan/CatanApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ogreten.catan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatanApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
