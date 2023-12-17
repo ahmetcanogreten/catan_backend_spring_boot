ffcfb3c2ff9fa4d0129f8aa96a5ab691d7ae7ad9
Ahmet Can Ogreten <ahmetcan.ogreten@kuartis.com>
2023-12-17 10:35:43 +0300

    Initial user controller test added

--- src/main/java/com/ogreten/catan/auth/schema/UserCreateIn.java
+++ src/main/java/com/ogreten/catan/auth/schema/UserCreateIn.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserCreateIn {
     @NotBlank(message = "Email is mandatory")
     @Email(message = "Email should be valid")
